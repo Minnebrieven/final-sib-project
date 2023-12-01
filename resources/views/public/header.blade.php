ba746be2fe060f6f15907bfe7ab8c293fbf6bc9f
Muarifrizqy <137586625+Muarifrizqy@users.noreply.github.com>
2023-12-01 13:27:16 +0700

    [progress] frontend28

---
+++ resources/views/public/header.blade.php
@@ -0,0 +1,26 @@
+<header id="header" class="top">
+    <div class="container d-flex align-items-center">
+        <h1 class="logo me-auto"><a href="index.html">Literasi Sampah</a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+        <nav id="navbar" class="navbar order-last order-lg-0">
+            <ul>
+                <li><a class="nav-link scrollto active" href="{{ ('/home') }}">Home</a></li>
+                <li><a class="nav-link scrollto" href="{{ ('/menu') }}">About</a></li>
+                <li><a class="nav-link scrollto" href="{{ ('/Tim') }}">Tim</a></li>
+                <li class="dropdown"><a href="#"><span>Menu</span> <i class="bi bi-chevron-down"></i></a>
+                    <ul>
+                        <li><a href="#">Penjualan</a></li>
+                        <li><a href="#">Berita</a></li>
+                    </ul>
+                </li>
+                <li><a class="nav-link scrollto" href="{{ ('/contact') }}">Contact</a></li>
+            </ul>
+            <i class="bi bi-list mobile-nav-toggle"></i>
+        </nav><!-- .navbar -->
+        <!-- <a href="private/index.blade.php" class="appointment-btn scrollto"><span class="d-none d-md-inline">Log</span> in</a> -->
+        <a href="{{ url('/dashboard') }}" class="appointment-btn scrollto"><span class="d-none d-md-inline">Log</span> in</a>
+
+    </div>
+</header><!-- End Header -->
---
+++ resources/views/public/home.blade.php
@@ -0,0 +1,62 @@
+@extends('public.index')
+@section('content')
+<!-- ======= Hero Section ======= -->
+<section id="hero" class="d-flex align-items-center">
+    <!-- <div class="container">
+        <h1>Welcome to Ruang Literasi Sampah</h1>
+        <h2>Pelayanan penjualan sampah serta informasi-informasi tentang sampah.</h2>
+        <a href="#about" class="btn-get-started scrollto">Register</a>
+    </div> -->
+</section><!-- End Hero -->
+
+<!-- ======= Why Us Section ======= -->
+<section id="why-us" class="why-us">
+    <div class="container">
+
+        <div class="row">
+            <div class="col-lg-4 d-flex align-items-stretch">
+                <div class="content">
+                    <h3>Sampah?</h3>
+                    <p>
+                        Sampah merupakan permasalahan yang sangat umum yang terjadi di masyarakat global.
+                        Sampah merupakan material sisa hasil aktivitas yang dibuang sebagai hasil dari proses produksi, baik itu dalam industri maupun rumah tangga.
+                        Dapat dikatakan sampah adalah sesuatu yang tidak diinginkan oleh manusia setelah proses dan penggunaannya berakhir..
+                    </p>
+                    <div class="text-center">
+                        <a href="menu" class="more-btn">Lanjut Baca <i class="bx bx-chevron-right"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-8 d-flex align-items-stretch">
+                <div class="icon-boxes d-flex flex-column justify-content-center">
+                    <div class="row">
+                        <div class="col-xl-4 d-flex align-items-stretch">
+                            <div class="icon-box mt-4 mt-xl-0">
+                                <i class="bx bx-receipt"></i>
+                                <h4>Berita</h4>
+                                <p>Consequuntur sunt aut quasi enim aliquam quae harum pariatur laboris nisi ut aliquip</p>
+                            </div>
+                        </div>
+                        <div class="col-xl-4 d-flex align-items-stretch">
+                            <div class="icon-box mt-4 mt-xl-0">
+                                <i class="bx bx-cube-alt"></i>
+                                <h4>Berita</h4>
+                                <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt</p>
+                            </div>
+                        </div>
+                        <div class="col-xl-4 d-flex align-items-stretch">
+                            <div class="icon-box mt-4 mt-xl-0">
+                                <i class="bx bx-images"></i>
+                                <h4>Berita</h4>
+                                <p>Aut suscipit aut cum nemo deleniti aut omnis. Doloribus ut maiores omnis facere</p>
+                            </div>
+                        </div>
+                    </div>
+                </div><!-- End .content-->
+            </div>
+        </div>
+
+    </div>
+</section>
+<!-- End Why Us Section -->
+@endsection
