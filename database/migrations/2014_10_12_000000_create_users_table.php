f57fc7bad1e1b16ab17f85f26bb052d36e3154b7
minnebrieven <libr.libr1711@gmail.com>
2023-12-08 15:42:13 +0700

    [bugfix] user migration not included role

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['admin', 'manager','staff','pelanggan'])->default('pelanggan');
+            $table->enum('isactive', ['yes', 'no','banned'])->default('yes');
+            $table->string('foto')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
