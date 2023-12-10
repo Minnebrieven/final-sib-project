13235d3c07103e8be7cd1d2f00c1709204cbf4cd
minnebrieven <libr.libr1711@gmail.com>
2023-12-10 09:34:39 +0700

    [feat][uwp] feature jual/beli sampah

--- app/Http/Controllers/TransaksiController.php
+++ app/Http/Controllers/TransaksiController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Transaksi;
 use App\Models\Sampah; //panggil model
 use App\Models\JenisSampah; //panggil model
+use App\Models\MetodePembayaran; //panggil model
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB; // jika pakai query builder
 use Illuminate\Database\Eloquent\Model;
@@ -28,7 +29,8 @@ class TransaksiController extends Controller
     {
         $arrayJenisSampah = JenisSampah::all();
         $arraySampah = Sampah::all();
-        return view('public.transaksi.form', compact('arraySampah', 'arrayJenisSampah'));
+        $arrayMetodePembayaran = MetodePembayaran::all();
+        return view('public.transaksi.form', compact('arraySampah', 'arrayJenisSampah', 'arrayMetodePembayaran'));
     }
 
     /**
@@ -36,7 +38,40 @@ class TransaksiController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate(
+            // column to validate and rules
+            [
+                'tipe_transaksi' => 'in:jual,beli',
+                'metode_pembayaran' => 'required|integer',
+                'sampah_id' => 'required|array',
+                'sampah_id.*' => 'required|integer',
+                'jumlah' => 'required|array',
+                'jumlah.*' => 'required|integer',
+            ],
+
+            //column custom errors
+            [
+                'tipe_transaksi.required' => 'tipe transaksi wajib diisi'
+            ]);
+            //lakukan insert data dari request form dgn query builder
+        try {
+            $now = DB::raw('CURRENT_TIMESTAMP');
+            $lastInsertedSampahID = DB::table('sampah')->insertGetId([
+                'nama' => $request->nama,
+                'jenis_sampah_id' => $request->jenis_sampah_id,
+                'satuan' => $request->satuan,
+                'harga' => $request->harga,
+                'created_at' => $now,
+                'updated_at' => $now
+            ]);
+
+            return redirect()->route('sampah.index')
+                ->with('success', 'Sampah baru berhasil ditambahkan ID:'.$lastInsertedSampahID);
+        } catch (\Exception $e) {
+            //return redirect()->back()
+            return redirect()->route('sampah.index')
+                ->with('error', 'Terjadi kesalahan saat input data!');
+        }
     }
 
     /**
