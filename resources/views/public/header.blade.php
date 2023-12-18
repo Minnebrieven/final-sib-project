472dd98463726653ece08d476dafe45a21f61776
Muarifrizqy <137586625+Muarifrizqy@users.noreply.github.com>
2023-12-18 22:13:33 +0700

    layouting

--- resources/views/public/header.blade.php
+++ resources/views/public/header.blade.php
@@ -6,16 +6,16 @@
 
         <nav id="navbar" class="navbar order-last order-lg-0">
             <ul>
-                <li><a class="nav-link scrollto" href="{{ ('/home') }}">Home</a></li>
-                <li><a class="nav-link scrollto active" href="{{ ('/menu') }}">About</a></li>
-                <li><a class="nav-link scrollto" href="{{ ('/Tim') }}">Tim</a></li>
+                <li><a class="nav-link scrollto {{ request()->is('home') ? 'active' : '' }}" href="{{ ('/home') }}">Home</a></li>
+                <li><a class="nav-link scrollto {{ request()->is('menu') ? 'active' : '' }}" href="{{ ('/menu') }}">About</a></li>
+                <li><a class="nav-link scrollto {{ request()->is('Tim') ? 'active' : '' }}" href="{{ ('/Tim') }}">Tim</a></li>
                 <li class="dropdown"><a href="#"><span>Menu</span> <i class="bi bi-chevron-down"></i></a>
                     <ul>
                         <li><a href="#">Penjualan</a></li>
                         <li><a href="#">Berita</a></li>
                     </ul>
                 </li>
-                <li><a class="nav-link scrollto" href="{{ ('/contact') }}">Contact</a></li>
+                <li><a class="nav-link scrollto {{ request()->is('contact') ? 'active' : '' }}" href="{{ ('/contact') }}">Contact</a></li>
             </ul>
             <i class="bi bi-list mobile-nav-toggle"></i>
         </nav><!-- .navbar -->
